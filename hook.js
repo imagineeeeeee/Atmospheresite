0d2a051575d04495285f8f9139adec235fbb85bf
imagineeeeeee <109488236+imagineeeeeee@users.noreply.github.com>
2023-12-11 13:18:30 +1030

    Update hook.js

--- hook.js
+++ hook.js
@@ -4,4 +4,82 @@ const addFieldButton = document.getElementById('add-field');
 const fieldsContainer = document.getElementById('fields-container');
 const sendMessageButton = document.getElementById('send-message');
 
-addWebhookButton.addEventListener
+let webhookUrl;
+let fields = [];
+
+addWebhookButton.addEventListener('click', () => {
+    webhookUrl = document.getElementById('webhook-url').value;
+    
+    if (webhookUrl) {
+        webhookDesigner.classList.remove('hidden');
+        document.getElementById('webhook-url').value = '';
+    } else {
+        alert('Please enter a valid webhook URL.');
+    }
+});
+
+addFieldButton.addEventListener('click', () => {
+    const fieldContainer = document.createElement('div');
+    fieldContainer.classList.add('field-container');
+
+    const fieldNameInput = document.createElement('input');
+    fieldNameInput.placeholder = 'Field Name';
+    fieldContainer.appendChild(fieldNameInput);
+
+    const fieldValueInput = document.createElement('input');
+    fieldValueInput.placeholder = 'Field Value';
+    fieldContainer.appendChild(fieldValueInput);
+
+    const removeFieldButton = document.createElement('button');
+    removeFieldButton.textContent = 'Remove Field';
+    removeFieldButton.addEventListener('click', () => {
+        fieldContainer.parentNode.removeChild(fieldContainer);
+    });
+    fieldContainer.appendChild(removeFieldButton);
+
+    fieldsContainer.appendChild(fieldContainer);
+});
+
+sendMessageButton.addEventListener('click', () => {
+    const messageContent = document.getElementById('message-content').value;
+
+    const embed = {
+        title: document.getElementById('embed-title').value,
+        color: document.getElementById('embed-color').value,
+        description: document.getElementById('embed-description').value,
+        footer: {
+            text: document.getElementById('embed-footer').value
+        },
+        author: {
+            name: document.getElementById('embed-author').value,
+            icon_url: document.getElementById('embed-author-image').value
+        },
+        fields: fields
+    };
+
+    fields = [];
+
+    for (const fieldContainer of document.querySelectorAll('.field-container')) {
+        fields.push({
+            name: fieldContainer.querySelector('input[placeholder="Field Name"]').value,
+            value: fieldContainer.querySelector('input[placeholder="Field Value"]').value
+        });
+    }
+
+    const data = {
+        embeds: [embed],
+        content: messageContent
+    };
+
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(() => {
+        alert('Message sent successfully!');
+    }).catch(error => {
+        alert('Error sending message:', error);
+    });
+});
