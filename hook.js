4458b44c0237e3a0642508a2a58d622b94384f70
imagineeeeeee <109488236+imagineeeeeee@users.noreply.github.com>
2023-12-11 13:15:57 +1030

    Create hook.js

---
+++ hook.js
@@ -0,0 +1,7 @@
+const addWebhookButton = document.getElementById('add-webhook');
+const webhookDesigner = document.getElementById('webhook-designer');
+const addFieldButton = document.getElementById('add-field');
+const fieldsContainer = document.getElementById('fields-container');
+const sendMessageButton = document.getElementById('send-message');
+
+addWebhookButton.addEventListener
