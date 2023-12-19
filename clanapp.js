69752b05b3051ffc1d8bcceb7106348e12d53602
imagineeeeeee <109488236+imagineeeeeee@users.noreply.github.com>
2023-12-19 18:07:25 +1030

    Create clanapp.js

---
+++ clanapp.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('quiz-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Access input data
+  const username = document.getElementById('username').value;
+  const gems = document.getElementById('gems').value;
+  const reason = document.getElementById('reason').value;
+  const contribution = document.getElementById('contribution').value;
+
+  // Validate data (can be customized further)
+  if (!username || !gems || !reason || !contribution) {
+    results.textContent = 'Please fill out all fields';
+    return;
+  }
+
+  // Build Discord webhook payload
+  const payload = {
+    content: `**Clan Application Received:**`,
+    embeds: [{
+      title: username,
+      description: `**Gems:** ${gems}\n**Reason for joining:** ${reason}\n**Contribution:** ${contribution}`,
+    }],
+  };
+
+  // Send data to Discord webhook
+  fetch('YOUR_WEBHOOK_URL', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      if (response.ok) {
+        results.textContent = 'Application submitted! Thanks for your interest.';
+      } else {
+        results.textContent = 'There was an error submitting your application. Please try again later.';
+      }
+    })
+    .catch((error) => {
+      results.textContent = `Error: ${error}`;
+    });
+
+  // Reset form (optional)
+  form.reset();
+});
