043bdf994361761e7f53debb4500b7b48c01d413
imagineeeeeee <109488236+imagineeeeeee@users.noreply.github.com>
2023-12-21 19:59:14 +1030

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-  const payload = {
-    content: `**Website user online.**`,
-    embeds: [{
-      title: `Website user on!)`,
-    }],
-  };
+const payload = {
+  content: `**Website user online.**`,
+  embeds: [{
+    title: `Website user on!)`,
+  }],
+};
 
   // Send data to Discord webhook
-  fetch('https://discord.com/api/webhooks/1187325343300849745/UzasQ47dTQnzx4Gdtfyy-IFzSzDxAe2tNz_l_f3iqW1hC6AXwywDLKXJoqs0XvluEU8x', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
+fetch('https://discord.com/api/webhooks/1187325343300849745/UzasQ47dTQnzx4Gdtfyy-IFzSzDxAe2tNz_l_f3iqW1hC6AXwywDLKXJoqs0XvluEU8x', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+   },
+  body: JSON.stringify(payload),
+})
