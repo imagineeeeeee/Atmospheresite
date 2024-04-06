d8adee60ed19cc5c23a1dc0d8d77081a0831ee03
imagineeeeeee <109488236+imagineeeeeee@users.noreply.github.com>
2024-04-06 15:55:36 +1030

    Update gb.py

--- gb.py
+++ gb.py
@@ -9,14 +9,9 @@ client = discord.Client(intents=discord.Intents.all())
 
 @app.route("/get_server_count")
 def get_server_count():
-    try:
-        # Connect to Discord using the securely retrieved bot token
-        client.run(BOT_TOKEN)  # This line will connect and block until disconnected
-        count = len(client.guilds)
-        return jsonify({"server_count": count})
-    except Exception as e:  # Catch any errors during connection or data retrieval
-        print(f"Error fetching server count: {e}")
-        return jsonify({"error": "Error fetching server count"}), 500  # Internal Server Error
+    count = len(client.guilds)
+    return jsonify({"server_count": count})
 
 if __name__ == "__main__":
     app.run()
+
