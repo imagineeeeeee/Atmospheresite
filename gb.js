3976d56f8192d9358d57533f308359417908b36a
imagineeeeeee <109488236+imagineeeeeee@users.noreply.github.com>
2024-04-06 15:22:03 +1030

    Create gb.js

---
+++ gb.js
@@ -0,0 +1,11 @@
+function updateServerCount() {
+    fetch("/get_server_count")
+        .then(response => response.json())
+        .then(data => {
+            const serverCountElement = document.getElementById("server-count");
+            serverCountElement.textContent = data.server_count;
+        });
+}
+
+// Call the function every few minutes to update the count
+setInterval(updateServerCount, 180000); // Update every 3 minutes
