383d6b5abb9bf6cb81fd1d070a76b0181491825e
imagineeeeeee <109488236+imagineeeeeee@users.noreply.github.com>
2024-04-06 15:48:50 +1030

    Update gb.js

--- gb.js
+++ gb.js
@@ -1,11 +1,22 @@
-function updateServerCount() {
-    fetch("/get_server_count")
-        .then(response => response.json())
-        .then(data => {
-            const serverCountElement = document.getElementById("server-count");
-            serverCountElement.textContent = data.server_count;
-        });
-}
+from flask import Flask, jsonify
+import discord
 
-// Call the function every few minutes to update the count
-setInterval(updateServerCount, 18000); // Update every 3 minutes
+BOT_TOKEN = "MTIxOTg2ODk2Mzg4Mjc5OTIxMw.GVDWff.ui1azi2Q0F8CSa1X5gcuidiocEGZrxiEdc81bk"
+
+app = Flask(__name__)
+
+client = discord.Client(intents=discord.Intents.all())
+
+@app.route("/get_server_count")
+def get_server_count():
+    try:
+        # Connect to Discord using the securely retrieved bot token
+        client.run(BOT_TOKEN)  # This line will connect and block until disconnected
+        count = len(client.guilds)
+        return jsonify({"server_count": count})
+    except Exception as e:  # Catch any errors during connection or data retrieval
+        print(f"Error fetching server count: {e}")
+        return jsonify({"error": "Error fetching server count"}), 500  # Internal Server Error
+
+if __name__ == "__main__":
+    app.run()
